04db132ae12cdd8d9f37b83fb5cb729e7444af81
league <league@imac-6.attlocal.net>
2018-04-14 12:26:13 -0700

    I created a robot that can create a square and a triangle

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot trumpbot = new Robot();
+trumpbot.penDown();
+trumpbot.setSpeed(32);
+for(int i=0;i<3;i++) {
+trumpbot.move(100);	
+trumpbot.turn (120);
 	
-		
-		
 	}
+}
 }
